6d860116a3fc3f1204626adbe71107d406ed9e83
Daniel Fernandes <danielfernandes@MacBook-Pro.local>
2020-09-06 11:19:27 -0400

    add os environ vars

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ from flask import Flask, request, jsonify, render_template, redirect, session, f
 from models import db, connect_db, NewBook, Inventory
 import requests
 import re
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///library'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
 
 connect_db(app)
 
