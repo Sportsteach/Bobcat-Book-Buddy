e9fb130bf60c51a9b9c722e2d62c775187673230
Daniel Fernandes <danielfernandes@MacBook-Pro.local>
2020-12-21 14:30:30 -0500

    Changes7

--- app.py
+++ app.py
@@ -8,18 +8,7 @@ import requests
 import re
 import os
 
-from flask import Flask
-
-my_awesome_app = Flask(__name__)
-
-
-@my_awesome_app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    my_awesome_app.run()
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgresql:///library')
